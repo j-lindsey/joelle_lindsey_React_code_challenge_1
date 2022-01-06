a8e4f201dcdc78529a6942bfeba3fe64660619bf
j-lindsey <j-lindsey@users.noreply.github.com>
2022-01-06 11:30:38 -0700

    added winner check and function

--- code_challenge/src/components/GameBoard/gameBoard.js
+++ code_challenge/src/components/GameBoard/gameBoard.js
@@ -17,32 +17,72 @@ class GameBoard extends React.Component {
             six: '',
             seven: '',
             eight: '',
-            nine: ''
+            nine: '',
+            winner: '',
+            disableBoard: false
         };
         this.handleClick = this.handleClick.bind(this);
+        this.checkForWinner = this.checkForWinner.bind(this);
 
     }
 
+    checkForWinner() {
+        let winner;
+        if ((this.state.one.iconName === 'times' && this.state.two.iconName === 'times' && this.state.three.iconName === 'times') || (this.state.one.iconName === 'circle' && this.state.two.iconName === 'circle' && this.state.three.iconName === 'circle')) {
+            winner = this.state.one.iconName;
+        } else if ((this.state.four.iconName === 'times' && this.state.five.iconName === 'times' && this.state.six.iconName === 'times') || (this.state.four.iconName === 'circle' && this.state.five.iconName === 'circle' && this.state.six.iconName === 'circle')) {
+            winner = this.state.four.iconName;
+        } else if ((this.state.seven.iconName === 'times' && this.state.eight.iconName === 'times' && this.state.nine.iconName === 'times') || (this.state.seven.iconName === 'circle' && this.state.eight.iconName === 'circle' && this.state.nine.iconName === 'circle')) {
+            winner = this.state.seven.iconName;
+        } else if ((this.state.one.iconName === 'times' && this.state.four.iconName === 'times' && this.state.seven.iconName === 'times') || (this.state.one.iconName === 'circle' && this.state.four.iconName === 'circle' && this.state.seven.iconName === 'circle')) {
+            winner = this.state.one.iconName;
+        } else if ((this.state.two.iconName === 'times' && this.state.five.iconName === 'times' && this.state.eight.iconName === 'times') || (this.state.two.iconName === 'circle' && this.state.five.iconName === 'circle' && this.state.eight.iconName === 'circle')) {
+            winner = this.state.two.iconName;
+        } else if ((this.state.three.iconName === 'times' && this.state.six.iconName === 'times' && this.state.nine.iconName === 'times') || (this.state.three.iconName === 'circle' && this.state.six.iconName === 'circle' && this.state.nine.iconName === 'circle')) {
+            winner = this.state.three.iconName;
+        } else if ((this.state.one.iconName === 'times' && this.state.five.iconName === 'times' && this.state.nine.iconName === 'times') || (this.state.one.iconName === 'circle' && this.state.five.iconName === 'circle' && this.state.nine.iconName === 'circle')) {
+            winner = this.state.one.iconName;
+        } else if ((this.state.three.iconName === 'times' && this.state.five.iconName === 'times' && this.state.seven.iconName === 'times') || (this.state.three.iconName === 'circle' && this.state.five.iconName === 'circle' && this.state.seven.iconName === 'circle')) {
+            winner = this.state.three.iconName;
+        } else {
+            winner = '';
+        }
+
+        if (winner === 'times') {
+            this.setState({ winner: this.props.player1 }, () => {
+                alert(`${this.state.winner} wins the game!`);
+                this.setState({ disableBoard: true });
+            })
+        }
+        else if (winner === 'circle') {
+            this.setState({ winner: this.props.player2 }, () => {
+                alert(`${this.state.winner} wins the game!`);
+                this.setState({ disableBoard: true });
+            })
+        }
+    }
+
+
+
     handleClick(event) {
         const name = event.target.id;
-        console.log(name);
-        console.log(this.state[name].iconName);
-        if (this.state[name].iconName === 'times' || this.state[name].iconName === 'circle') {
-            alert('Please select an open square.')
-        } else {
-            if (this.props.currentPlayer === this.props.player1) {
-                this.setState({ [name]: faTimes })
+        if (this.state.disableBoard === false) {
+            if (this.state[name].iconName === 'times' || this.state[name].iconName === 'circle') {
+                alert('Please select an open square.')
             } else {
-                this.setState({ [name]: faCircle })
+                if (this.props.currentPlayer === this.props.player1) {
+                    this.setState({ [name]: faTimes }, this.checkForWinner)
+                } else {
+                    this.setState({ [name]: faCircle }, this.checkForWinner)
+                }
+                this.props.changeCurrentPlayer();
             }
-        
-            this.props.changeCurrentPlayer();
         }
     }
 
     render() {
         const { one, two, three, four, five, six, seven, eight, nine } = this.state;
-        
+
         return (
             <div className="gameBoard-main">
                 <div className="row">
